ce4055d8b4820028c1dfd6af05dda1be1e0e096a
Haytham Salah <haytham.salah@gmail.com>
2021-12-13 19:19:14 +0200

    Removing an unneeded comment from the source code

--- Simple/src/app/App.java
+++ Simple/src/app/App.java
@@ -1,7 +1,5 @@
 package app;
 
-// This is an arbitrary pointless comment.
-
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World: Inside the APP !!!");
