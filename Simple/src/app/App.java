24fa68f1438c77fd08184c16a0e871e14fa50e4a
Haytham Salah <haytham.salah@gmail.com>
2021-12-13 19:14:41 +0200

    Committing a new comment in the source code

--- Simple/src/app/App.java
+++ Simple/src/app/App.java
@@ -1,5 +1,7 @@
 package app;
 
+// This is an arbitrary pointless comment.
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World: Inside the APP !!!");
